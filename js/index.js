92664d22f8566e50bc94f1fda118a927f089712d
Ching-yu-25 <ycy0227@gmail.com>
2023-02-27 17:30:13 +0800

    update card

--- js/index.js
+++ js/index.js
@@ -1,20 +1,39 @@
+function heartShow() {
+  if (!$(".photos").hasClass("heart-show")) {
+    $(".heart,.photos").addClass("heart-show");
+  } else {
+    $(".heart,.photos").removeClass("heart-show");
+  }
+}
+
+function checkPsw(psw) {
+  let encodepsw;
+  encodepsw = md5(psw);
+  encodepsw == "f5c838c78c86699fa2c3d9f8703a051b" ? $(".cover").fadeOut() : $("#psw").val('').addClass("wrong");
+}
+
 $(document).ready(function () {
   $('.photos').slick({
     dots: true,
   });
-})
 
-$(".heart").click(function () {
-  if (!$(".photos").hasClass("heart-show")) {
-    $(this).addClass("heart-show");
-    $(".photos").addClass("heart-show");
-  } else {
-    $(this).removeClass("heart-show");
-    $(".photos").removeClass("heart-show");
-  }
 
-})
+  $(".heart").click(heartShow);
 
-$(".read-more").click(function () {
-  $(".text-inner").addClass("show-more");
+  $(".photos").dblclick(heartShow);
+
+  $(".read-more").click(function () {
+    $(".text-inner").addClass("show-more");
+    $(".read-more").hide();
+  })
+
+  $("#psw").on("keyup", function (e) {
+    if (e.which === 13) {
+      checkPsw($(this).val());
+    }
+  })
+
+  $("#openbtn").on("click", function () {
+    checkPsw($("#psw").val())
+  });
 })
